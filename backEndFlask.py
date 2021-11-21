f335844550c7d5cd305f8a25b88b90f5192ddd05
Zhen Wu <zwu25@uci.edu>
2021-11-20 17:31:09 -0800

    made 2nd gamewebsite. Cashier buttons and display now works :)

--- backEndFlask.py
+++ backEndFlask.py
@@ -3,12 +3,19 @@ from flask import request
 from flask_cors import CORS
 
 
-
 app = Flask(__name__)
 CORS(app)
 
 
-diffculty = "NOT SET"
+difficulty = "NOT SET"
+CurrentMoneyPicked = 0
+
+
+def string_Difficult_to_int(str):
+    # currently impossible is the same as easy
+    mapping = {"Easy":0,"Medium":1,"Hard":2,"Impossible":0}
+    return mapping.get(str)
+
 
 @app.route("/")
 def hello_world():
@@ -17,5 +24,32 @@ def hello_world():
 
 @app.route("/update_difficulty",methods=["POST"])
 def update_difficulty():
+    global difficulty
+    difficulty = string_Difficult_to_int(request.form['difficulty'])
     print(request.form['difficulty'])
     return "200"
+
+
+@app.route("/picked_more_money",methods=["POST"])
+def picked_more_money():
+    global CurrentMoneyPicked
+    print('picked more money')
+    CurrentMoneyPicked += float(request.form['dollarAmount'])
+    CurrentMoneyPicked = round(CurrentMoneyPicked, 2)
+
+    print(CurrentMoneyPicked)
+    return "200"
+
+
+@app.route("/submit_and_clear",methods=["POST"])
+def submit_and_clear():
+    global CurrentMoneyPicked
+    # link with game here
+    CurrentMoneyPicked = 0
+    return "200"
+
+
+@app.route("/return_current_money_picked",methods=["POST"])
+def return_current_money_picked():
+    global CurrentMoneyPicked
+    return str(CurrentMoneyPicked)
--- indexjs.js
+++ indexjs.js
@@ -1,8 +1,6 @@
 async function sendDifficulty(event) {
     event.preventDefault();
 
-    console.log("test")
-
     const formData = new FormData();
 
 var select = document.getElementById('difficulty');
@@ -16,11 +14,59 @@ var value = select.options[select.selectedIndex].value;
     }).then(response => response.text())
     .then(data => {
         if (data === "200") {
-            alert("success");
+            console.log("success");
         } else {
-            alert("try again")
+            console.log("try again")
         }
     });
+
+    return;
+}
+
+async function addMoney(event,denomination) {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append("dollarAmount",denomination)
+
+    await fetch("http://127.0.0.1:5000/picked_more_money", {
+        method: 'POST',
+        body: formData
+    }).then(response => response.text())
+    .then(data => {
+        if (data === "200") {
+            console.log("success");
+        } else {
+            console.log("try again")
+        }
+    });
+    returnCashUpdate()
+    return;
+}
+
+async function returnCash(event) {
+    event.preventDefault();
+
+    await fetch("http://127.0.0.1:5000/submit_and_clear", {
+        method: 'POST',
+    }).then(response => response.text())
+    .then(data => {
+        returnCashUpdate()
+    });
+
+    return;
+}
+
+async function returnCashUpdate() {
+    // instead of being static it should call python flask server
+    await fetch("http://127.0.0.1:5000/return_current_money_picked", {
+        method: 'POST',
+    }).then(response => response.text())
+    .then(data => {
+       document.getElementById("moneyReturnAmount").innerHTML = data;
+    });
+
+
 
     return;
 }
